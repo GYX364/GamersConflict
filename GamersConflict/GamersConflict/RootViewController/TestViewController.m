76e7e504ef41b60f6e05b672badbe3156ddf3ca2
GYX364 <18563981716@163.com>
2016-04-18 20:43:55 +0800

    测试Button动画

--- GamersConflict/GamersConflict/RootViewController/TestViewController.m
+++ GamersConflict/GamersConflict/RootViewController/TestViewController.m
@@ -7,16 +7,63 @@
 //
 
 #import "TestViewController.h"
-
+#import "GCCircularButton.h"
 @interface TestViewController ()
-
+@property (nonatomic, strong)GCCircularButton *button1;
+@property (nonatomic, strong)GCCircularButton *button2;
+@property (nonatomic, strong)GCCircularButton *button3;
 @end
 
 @implementation TestViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    GCCircularButton *button = [[GCCircularButton alloc]initWithRadius:60.0 origin:CGPointMake(100, 100)];
+    button.backgroundColor = [UIColor blackColor];
+    
+    [button addTarget:self action:@selector(bAction:) forControlEvents:(UIControlEventTouchUpInside)];
+    self.button1 = [[GCCircularButton alloc]initWithRadius:40 origin:CGPointMake(100, 100)];
+    self.button2 = [[GCCircularButton alloc]initWithRadius:40 origin:CGPointMake(100, 100)];
+    self.button3 = [[GCCircularButton alloc]initWithRadius:40 origin:CGPointMake(100, 100)];
+    self.button1.backgroundColor = [UIColor grayColor];
+    self.button2.backgroundColor = [UIColor cyanColor];
+    self.button3.backgroundColor = [UIColor redColor];
+    [self.view addSubview:button];
+    [self.view addSubview:self.button1];
+    [self.view addSubview:self.button2];
+    [self.view addSubview:self.button3];
+    
     // Do any additional setup after loading the view from its nib.
+}
+
+- (void)bAction:(UIButton *)sender{
+    self.view.backgroundColor = [UIColor colorWithRed:arc4random()%256 / 255.0 green:arc4random()%256/255.0 blue:arc4random()%256/ 255.0 alpha:1];
+//    [UIColor colorWithRed:arc4random() % 256 / 255.0 green:arc4random()%256 / 255.0 blue:arc4random()%256/255.0 alpha:1];
+    [self open];
+    
+}
+
+- (void)open{
+    [UIView animateKeyframesWithDuration:1 delay:0.2 options:(0   ) animations:^{
+        // 逐个添加关键帧
+        [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.25 animations:^{
+           CGPoint point1 = CGPointMake(100, 75);
+//            self.button1.frame.origin = point1;
+            self.button1.frame = CGRectMake(100, 75, 40, 40);
+            
+        }];
+        [UIView addKeyframeWithRelativeStartTime:0.3 relativeDuration:0.25 animations:^{
+            CGPoint point2 = CGPointMake(125, 100);
+//            self.button2.frame.origin = point2;
+            self.button2.frame = CGRectMake(125, 100, 40, 40);
+            
+        }];
+        [UIView addKeyframeWithRelativeStartTime:0.6 relativeDuration:0.25 animations:^{
+            self.button3.frame = CGRectMake(100, 125, 40, 40);
+        }];
+    } completion:^(BOOL finished) {
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
